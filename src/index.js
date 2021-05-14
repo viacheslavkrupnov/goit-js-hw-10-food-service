64b5e6e97d3f85babb243347739f66308f26ca12
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-05-14 11:26:28 +0400

    add script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import menuElement from './menu.json'
+import menuTemplate from './templates/menu-items.hbs';
+
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+    const markup = array.map(post => menuTemplate(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
